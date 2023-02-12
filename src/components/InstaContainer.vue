9babc1e3f1d8ea8207f08721f3921b8c9ff58435
김명욱 <anko@gsitm.com>
2023-02-12 20:58:31 +0900

    post 기능

---
+++ src/components/InstaContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Post 
+      v-for="(item, index) in items" :key="index"
+      :name="item.name" 
+      :userImage="item.userImage" 
+      :postImage="item.postImage" 
+      :date="item.date"
+      :liked="item.liked" 
+      :likes="item.likes" 
+      :content="item.content" 
+      :filter="item.filter"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import Post from './InstaPost.vue';
+
+defineProps({
+  items: Array,
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/InstaPost.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div class="profile" :style="{ backgroundImage: `url(${userImage})` }"></div>
+      <span class="profile-name">{{ name }}</span>
+    </div>
+    <div class="post-body" :style="{ backgroundImage: `url(${postImage})` }"></div>
+    <div class="post-content">
+      <p>{{ likes }} Likes</p>
+      <p><strong>{{ filter }}</strong> {{ content }}</p>
+      <p class="date">{{ date }}</p>
+    </div>
+  </div> 
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  name: String,
+  userImage: String,
+  postImage: String,
+  likes: [String, Number],
+  date: String,
+  liked: Boolean,
+  content: String,
+  filter: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.post {
+  width: 100%;
+}
+.profile {
+  // background-image: url("https://placeimg.com/100/100/arch");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  // background-image: url("https://placeimg.com/640/480/animals");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+} 
+</style>
